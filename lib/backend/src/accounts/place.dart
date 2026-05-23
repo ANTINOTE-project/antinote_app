fec28ccc7b1a43aa42a885b6e25f655940afb8cd
Haroun El Omri <h.elomri@proton.me>
2026-05-23 21:42:52 +0200

    Refactoring

---
+++ lib/backend/src/accounts/place.dart
@@ -0,0 +1,75 @@
+import "dart:convert";
+
+import "package:antinote/antinote.dart";
+import "package:http/http.dart" as http;
+
+enum PlaceType {
+  city,
+  town,
+  village,
+  hamlet,
+  suburb,
+  municipality,
+  other;
+
+  static PlaceType fromString(String? value) => switch (value) {
+    "city" => city,
+    "town" => town,
+    "village" => village,
+    "hamlet" => hamlet,
+    "suburb" => suburb,
+    "municipality" => municipality,
+    _ => other,
+  };
+}
+
+class City {
+  final String name;
+  final double latitude;
+  final double longitude;
+  final String postalCode;
+  final PlaceType placeType;
+
+  const City({
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+    required this.postalCode,
+    this.placeType = PlaceType.other,
+  });
+
+  static Future<List<City>> fetchCitiesAroundPlace(String query) async {
+    final uri = Uri.https("nominatim.openstreetmap.org", "/search", {
+      "q": query,
+      "format": "json",
+      "limit": "15",
+      "countrycodes": "fr",
+      "featuretype": "settlement",
+      "addressdetails": "1",
+    });
+
+    final response = await http.get(
+      uri,
+      headers: {"User-Agent": "Antinote/1.0"},
+    );
+
+    try {
+      return (jsonDecode(response.body) as ListJsonNavigator<MapJsonNavigator>)
+          .mapL((e) => e.asCity());
+    } catch (_) {
+      return [];
+    }
+  }
+}
+
+extension AsCity on MapJsonNavigator {
+  City asCity() {
+    return City(
+      name: get("name"),
+      latitude: get("lat"),
+      longitude: get("lon"),
+      postalCode: getM("address").get("postcode"),
+      placeType: PlaceType.fromString(get("addresstype")),
+    );
+  }
+}
--- lib/frontend/screens/auth/search/city.dart
+++ lib/frontend/screens/auth/search/city.dart
@@ -1,6 +1,6 @@
 import "dart:async";
-import "dart:convert";
 
+import "package:antinote_app/backend/src/accounts/place.dart";
 import "package:antinote_app/frontend/extensions/colors.dart";
 import "package:antinote_app/frontend/extensions/l10n.dart";
 import "package:antinote_app/frontend/routing/routes.dart";
@@ -9,66 +9,7 @@ import "package:antinote_app/frontend/widgets/customs/field.dart";
 import "package:antinote_app/frontend/widgets/pressable.dart";
 import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
-import "package:http/http.dart" as http;
 import "package:hugeicons_pro/hugeicons.dart";
-
-enum PlaceType {
-  city,
-  town,
-  village,
-  hamlet,
-  suburb,
-  municipality,
-  other;
-
-  static PlaceType fromString(String? value) => switch (value) {
-    "city" => city,
-    "town" => town,
-    "village" => village,
-    "hamlet" => hamlet,
-    "suburb" => suburb,
-    "municipality" => municipality,
-    _ => other,
-  };
-
-  IconData get icon => switch (this) {
-    city => HugeIconsSolid.building01,
-    town => HugeIconsSolid.building02,
-    village => HugeIconsSolid.home01,
-    hamlet => HugeIconsSolid.house01,
-    suburb => HugeIconsSolid.house04,
-    municipality => HugeIconsSolid.city01,
-    other => HugeIconsSolid.location01,
-  };
-}
-
-class City {
-  final String name;
-  final double latitude;
-  final double longitude;
-  final String postalCode;
-  final PlaceType placeType;
-
-  const City({
-    required this.name,
-    required this.latitude,
-    required this.longitude,
-    required this.postalCode,
-    this.placeType = PlaceType.other,
-  });
-
-  factory City.fromJson(Map<String, dynamic> json) {
-    return City(
-      name: json["name"] as String,
-      latitude: double.parse(json["lat"] as String),
-      longitude: double.parse(json["lon"] as String),
-      postalCode: json["display_name"] != null
-          ? RegExp(r"\b\d{5}\b").firstMatch(json["display_name"] as String)?.group(0) ?? ""
-          : "",
-      placeType: PlaceType.fromString(json["addresstype"] as String?),
-    );
-  }
-}
 
 class LoginSearchCityScreen extends StatefulWidget {
   const LoginSearchCityScreen({super.key});
@@ -106,20 +47,7 @@ class _LoginSearchCityScreenState extends State<LoginSearchCityScreen> {
   Future<void> _searchCities(String query) async {
     if (query.length <= 3) return;
 
-    final uri = Uri.https("nominatim.openstreetmap.org", "/search", {
-      "q": query,
-      "format": "json",
-      "limit": "15",
-      "countrycodes": "fr",
-      "featuretype": "settlement",
-    });
-
-    final response = await http.get(uri, headers: {"User-Agent": "Antinote/1.0"});
-    final data = jsonDecode(response.body) as List;
-
-    if (mounted) {
-      setState(() => _cities = data.map((e) => City.fromJson(e)).toList());
-    }
+    _cities = await City.fetchCitiesAroundPlace(query);
   }
 
   @override
@@ -169,24 +97,41 @@ class _LoginSearchCityScreenState extends State<LoginSearchCityScreen> {
 
                 child: Card(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 16,
+                    ),
 
                     child: Row(
                       spacing: 10,
 
                       children: [
-                        Icon(city.placeType.icon),
+                        Icon(switch (city.placeType) {
+                          .city => HugeIconsSolid.building01,
+                          .town => HugeIconsSolid.building02,
+                          .village => HugeIconsSolid.home01,
+                          .hamlet => HugeIconsSolid.house01,
+                          .suburb => HugeIconsSolid.house04,
+                          .municipality => HugeIconsSolid.city01,
+                          .other => HugeIconsSolid.location01,
+                        }),
 
                         Expanded(
                           child: Text(
                             city.name,
-                            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
 
                         Text(
                           city.postalCode,
-                          style: TextStyle(color: context.c.inversePrimary, fontWeight: FontWeight.w500),
+                          style: TextStyle(
+                            color: context.c.inversePrimary,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ],
                     ),
