325991905f6df3841fa35810202e900c77437d30
LeChacal <dev.lechacal@gmail.com>
2026-05-23 11:16:04 +0200

    Moved to auth (was login)

--- lib/frontend/routing/router.dart
+++ lib/frontend/routing/router.dart
@@ -1,11 +1,15 @@
 import "package:antinote_app/frontend/app.dart";
 import "package:antinote_app/frontend/routing/routes.dart";
-import "package:antinote_app/frontend/screens/login/screen.dart";
+import "package:antinote_app/frontend/screens/auth/pick.dart";
+import "package:antinote_app/frontend/screens/auth/login.dart";
 import "package:go_router/go_router.dart";
 
-GoRouter makeRouter({String initialLocation = "/"}) => GoRouter(
+GoRouter makeRouter({String initialLocation = Routes.home}) => GoRouter(
   initialLocation: initialLocation,
   navigatorKey: App.navigatorKey,
 
-  routes: [GoRoute(path: Routes.auth.login, builder: (_, _) => const LoginScreen())],
+  routes: [
+    GoRoute(path: Routes.auth.login, builder: (_, _) => const LoginScreen()),
+    GoRoute(path: Routes.auth.pick, builder: (_, _) => const LoginPickScreen()),
+  ],
 );
