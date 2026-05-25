6b42418ebb0d8463c7c13f429dc04f4770bded37
LeChacal <dev.lechacal@gmail.com>
2026-05-25 23:09:54 +0200

    Added loading on home page

--- lib/frontend/screens/shell/screens/home.dart
+++ lib/frontend/screens/shell/screens/home.dart
@@ -1,10 +1,30 @@
+import "dart:async";
+
+import "package:antinote/antinote.dart";
+import "package:antinote_app/frontend/screens/screen.dart";
+import "package:antinote_app/frontend/widgets/customs/loading.dart";
 import "package:flutter/material.dart";
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with ScreenMixin<HomeScreen> {
+  @override
+  Widget buildLoaded(BuildContext context, RefreshIndicatorBuilder buildRefreshIndicator) {
+    return buildRefreshIndicator(child: const Center(child: LoadingWidget(size: 30)));
+  }
+
+  @override
+  Widget buildLoading(BuildContext context, RefreshIndicatorBuilder buildRefreshIndicator) {
+    return buildRefreshIndicator(child: const Center(child: LoadingWidget(size: 30)));
+  }
+
+  @override
+  FutureOr<void> loadActiveDataFromSession(PronoteSession session) {
+    // TODO: implement loadActiveDataFromSession
   }
 }
