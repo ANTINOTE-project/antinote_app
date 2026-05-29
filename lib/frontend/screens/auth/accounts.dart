22e71213cd7df126ee4df25ef6c1ba6d0fd4c9aa
LeChacal <dev.lechacal@gmail.com>
2026-05-29 21:25:20 +0200

    Using new is loading parameters

--- lib/frontend/screens/auth/accounts.dart
+++ lib/frontend/screens/auth/accounts.dart
@@ -7,12 +7,12 @@ import "package:antinote_app/frontend/widgets/animated/icon.dart";
 import "package:antinote_app/frontend/widgets/customs/app_bar.dart";
 import "package:antinote_app/frontend/widgets/customs/button.dart";
 import "package:antinote_app/frontend/widgets/customs/list.dart";
-import "package:antinote_app/frontend/widgets/customs/loading.dart";
 import "package:antinote_app/main.dart";
 import "package:antinote_app/protos/account.pb.dart";
 import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
 import "package:hugeicons_pro/hugeicons.dart";
+import "package:skeletonizer/skeletonizer.dart";
 
 class AccountsScreen extends StatefulWidget {
   const AccountsScreen({super.key});
@@ -23,6 +23,15 @@ class AccountsScreen extends StatefulWidget {
 
 class _AccountsScreenState extends State<AccountsScreen>
     with WidgetsBindingObserver {
+  static final List<AntinoteAccount> _fakeAccounts = List.filled(
+    5,
+    AntinoteAccount(
+      name: "fake account name",
+      establishmentName: "fake establishment",
+      workspaceName: "fake workspace",
+    ),
+  );
+
   List<AntinoteAccount>? _accounts;
 
   String? _defaultUid;
@@ -142,139 +151,135 @@ class _AccountsScreenState extends State<AccountsScreen>
           ),
         ),
 
-        body: _accounts == null
-            ? const Center(child: LoadingWidget(size: 30))
-            : Padding(
-                padding: const EdgeInsets.only(left: 12, right: 12, bottom: 70),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 12, right: 12, bottom: 70),
 
-                child: CustomScrollView(
-                  slivers: [
-                    ListWidget(
-                      isLoading: _accounts == null,
-                      items: _accounts!,
+          child: CustomScrollView(
+            slivers: [
+              ListWidget(
+                items: _accounts == null ? _fakeAccounts : _accounts!,
+                isLoading: _accounts == null,
 
-                      itemBuilder: (context, account, borderRadius) {
-                        return ItemWidget(
-                          borderRadius: borderRadius,
+                itemBuilder: (context, account, borderRadius) {
+                  return ItemWidget(
+                    borderRadius: borderRadius,
 
-                          onPressed: () => _onAccountPressed(account),
+                    onPressed: () => _onAccountPressed(account),
 
-                          onLongPress: () async {
-                            await showModalBottomSheet(
-                              context: context,
+                    onLongPress: () async {
+                      await showModalBottomSheet(
+                        context: context,
 
-                              builder: (context) {
-                                return SafeArea(
-                                  child: Padding(
-                                    padding: MediaQuery.viewInsetsOf(context),
+                        builder: (context) {
+                          return SafeArea(
+                            child: Padding(
+                              padding: MediaQuery.viewInsetsOf(context),
 
-                                    child: Container(
-                                      padding: const EdgeInsets.symmetric(
-                                        horizontal: 16,
-                                        vertical: 14,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 14,
+                                ),
+
+                                child: Column(
+                                  mainAxisSize: .min,
+                                  spacing: 8,
+
+                                  children: [
+                                    if (_defaultUid == account.uid)
+                                      ButtonWidget(
+                                        onPressed: () async {
+                                          await context.as.setDefault(null);
+                                          await _load();
+
+                                          if (context.mounted) {
+                                            context.pop();
+                                          }
+                                        },
+
+                                        label: context.l10n.disableAutoLogin,
+                                      )
+                                    else
+                                      ButtonWidget(
+                                        onPressed: () async {
+                                          await context.as.setDefault(
+                                            account.uid,
+                                          );
+                                          await _load();
+
+                                          if (context.mounted) {
+                                            context.pop();
+                                          }
+                                        },
+
+                                        label: context.l10n.enableAutoLogin,
                                       ),
 
-                                      child: Column(
-                                        mainAxisSize: .min,
-                                        spacing: 8,
+                                    ButtonWidget(
+                                      onPressed: () async {
+                                        await context.as.deleteAccount(
+                                          account.uid,
+                                        );
+                                        await _load();
 
-                                        children: [
-                                          if (_defaultUid == account.uid)
-                                            ButtonWidget(
-                                              onPressed: () async {
-                                                await context.as.setDefault(
-                                                  null,
-                                                );
-                                                await _load();
+                                        if (context.mounted) {
+                                          context.pop();
+                                        }
+                                      },
 
-                                                if (context.mounted) {
-                                                  context.pop();
-                                                }
-                                              },
-
-                                              label:
-                                                  context.l10n.disableAutoLogin,
-                                            )
-                                          else
-                                            ButtonWidget(
-                                              onPressed: () async {
-                                                await context.as.setDefault(
-                                                  account.uid,
-                                                );
-                                                await _load();
-
-                                                if (context.mounted) {
-                                                  context.pop();
-                                                }
-                                              },
-
-                                              label:
-                                                  context.l10n.enableAutoLogin,
-                                            ),
-
-                                          ButtonWidget(
-                                            onPressed: () async {
-                                              await context.as.deleteAccount(
-                                                account.uid,
-                                              );
-                                              await _load();
-
-                                              if (context.mounted) {
-                                                context.pop();
-                                              }
-                                            },
-
-                                            isDangerous: true,
-                                            icon: HugeIconsSolid.delete02,
-                                            label: context.l10n.delete,
-                                          ),
-                                        ],
-                                      ),
+                                      isDangerous: true,
+                                      icon: HugeIconsSolid.delete02,
+                                      label: context.l10n.delete,
                                     ),
-                                  ),
-                                );
-                              },
-                            );
-                          },
-
-                          leading: IconWidget(
-                            size: 28,
-                            iconOn: HugeIconsSolid.star,
-                            iconOff: HugeIconsSolid.userAccount,
-                            value: _defaultUid == account.uid,
-                          ),
-
-                          title: Text(
-                            account.name,
-                            style: const TextStyle(fontSize: 20),
-                          ),
-
-                          subtitle: Column(
-                            crossAxisAlignment: .start,
-
-                            children: [
-                              Text(
-                                account.establishmentName,
-                                style: const TextStyle(fontSize: 14),
+                                  ],
+                                ),
                               ),
+                            ),
+                          );
+                        },
+                      );
+                    },
 
-                              Text(
-                                account.workspaceName,
-                                style: const TextStyle(fontSize: 14),
-                              ),
-                            ],
-                          ),
-
-                          trailing: Icon(
-                            HugeIconsSolid.arrowRight01,
-                            color: context.c.outline,
-                          ),
-                        );
-                      },
+                    leading: IconWidget(
+                      size: 28,
+                      iconOn: HugeIconsSolid.star,
+                      iconOff: HugeIconsSolid.userAccount,
+                      value: _defaultUid == account.uid,
                     ),
-                  ],
-                ),
+
+                    title: Text(
+                      account.name,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+
+                    subtitle: Column(
+                      crossAxisAlignment: .start,
+
+                      children: [
+                        Text(
+                          account.establishmentName,
+                          style: const TextStyle(fontSize: 14),
+                        ),
+
+                        Text(
+                          account.workspaceName,
+                          style: const TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+
+                    trailing: Skeleton.ignore(
+                      child: Icon(
+                        HugeIconsSolid.arrowRight01,
+                        color: context.c.outline,
+                      ),
+                    ),
+                  );
+                },
               ),
+            ],
+          ),
+        ),
       ),
     );
   }
