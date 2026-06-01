6b91ac0f3a17680fea14d0195057b752eea582b4
LeChacal <dev.lechacal@gmail.com>
2026-06-01 08:14:21 +0200

    Moved theme.dart

--- lib/frontend/app.dart
+++ lib/frontend/app.dart
@@ -1,5 +1,5 @@
 import "package:antinote_app/frontend/routing/router.dart";
-import "package:antinote_app/frontend/theme/theme.dart";
+import "package:antinote_app/frontend/theme.dart";
 import "package:antinote_app/l10n/app_localizations.dart";
 import "package:flutter/material.dart";
 import "package:flutter_localizations/flutter_localizations.dart";
