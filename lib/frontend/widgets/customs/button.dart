2bd583ade7e1f05fe4b3efd615bbb7a1b90ed944
LeChacal <dev.lechacal@gmail.com>
2026-06-04 17:23:31 +0200

    Removed all animations on button widget

--- lib/frontend/widgets/customs/button.dart
+++ lib/frontend/widgets/customs/button.dart
@@ -1,4 +1,3 @@
-import "package:antinote_app/frontend/widgets/animated/icon.dart";
 import "package:antinote_app/frontend/widgets/customs/loading.dart";
 import "package:antinote_app/frontend/widgets/pressable.dart";
 import "package:antinote_app/utils.dart";
@@ -28,9 +27,6 @@ class ButtonWidget extends StatelessWidget {
     this.isEnabled = true,
   });
 
-  static const _duration = Duration(milliseconds: 300);
-  static const _curve = Curves.easeInOut;
-
   Future<void> _onPressed(BuildContext context) async {
     if (!isEnabled || isLoading) return;
     onPressed.call();
@@ -56,121 +52,50 @@ class ButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Pressable(
-      borderRadius: const BorderRadius.all(Radius.circular(16)),
+      borderRadius: const .all(.circular(16)),
 
       onPressed: () => _onPressed(context),
       hasFeedback: isEnabled || isLoading,
 
       child: Ink(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        padding: const .symmetric(horizontal: 16, vertical: 8),
 
         width: double.infinity,
         height: 48,
 
         decoration: BoxDecoration(
           color: _getButtonColor(context),
-          borderRadius: const BorderRadius.all(Radius.circular(16)),
+          borderRadius: const .all(.circular(16)),
         ),
 
         child: Center(
-          child: AnimatedSwitcher(
-            duration: _duration,
+          child: isLoading
+              ? const LoadingWidget(size: 24)
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 6,
 
-            switchInCurve: _curve,
-            switchOutCurve: _curve,
+                  children: [
+                    if (icon != null)
+                      Icon(icon!, color: _getTextColor(context), size: 24)
+                    else
+                      const SizedBox.shrink(),
 
-            layoutBuilder: (currentChild, previousChildren) {
-              return Stack(
-                alignment: Alignment.center,
-                children: [...previousChildren, ?currentChild],
-              );
-            },
-
-            transitionBuilder: (child, animation) {
-              final isIncoming = animation.status != AnimationStatus.reverse;
-
-              final slideAnimation =
-                  Tween<Offset>(
-                    begin: isIncoming
-                        ? const Offset(0, 0.5)
-                        : const Offset(0, -0.5),
-                    end: Offset.zero,
-                  ).animate(
-                    CurvedAnimation(
-                      parent: animation,
-                      curve: Curves.easeOutBack,
-                    ),
-                  );
-
-              final fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-                CurvedAnimation(parent: animation, curve: Curves.easeInOut),
-              );
-
-              final scaleAnimation = Tween<double>(begin: 0.6, end: 1.0)
-                  .animate(
-                    CurvedAnimation(
-                      parent: animation,
-                      curve: Curves.easeOutBack,
-                    ),
-                  );
-
-              return FadeTransition(
-                opacity: fadeAnimation,
-                child: SlideTransition(
-                  position: slideAnimation,
-                  child: ScaleTransition(scale: scaleAnimation, child: child),
+                    if (label != null)
+                      Text(
+                        label!,
+                        style: TextStyle(
+                          color: _getTextColor(context),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      )
+                    else
+                      const SizedBox.shrink(),
+                  ],
                 ),
-              );
-            },
-
-            child: _buildChild(context),
-          ),
         ),
       ),
-    );
-  }
-
-  Widget _buildChild(BuildContext context) {
-    return isLoading ? _buildLoading() : _buildIconAndLabel(context);
-  }
-
-  Widget _buildLoading() {
-    return const LoadingWidget(size: 24);
-  }
-
-  Widget _buildIconAndLabel(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      spacing: 6,
-      children: [_buildIcon(context), _buildLabel(context)],
-    );
-  }
-
-  Widget _buildIcon(BuildContext context) {
-    if (icon == null) return const SizedBox.shrink();
-
-    return IconWidget(
-      iconOn: icon!,
-      iconOff: icon!,
-      colorOn: _getTextColor(context),
-      value: true,
-    );
-  }
-
-  Widget _buildLabel(BuildContext context) {
-    if (label == null) return const SizedBox.shrink();
-
-    return AnimatedDefaultTextStyle(
-      style: TextStyle(
-        color: _getTextColor(context),
-        fontSize: 16,
-        fontWeight: FontWeight.bold,
-      ),
-
-      duration: _duration,
-      curve: _curve,
-
-      child: Text(label ?? ""),
     );
   }
 }
