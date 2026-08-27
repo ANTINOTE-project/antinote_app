1724dbb65ea2902721e5d733a42213eb492c91e6
Haroun El Omri <h.elomri@proton.me>
2026-08-27 18:48:48 +0200

    Made multi-configuration blocks look nicer

--- lib/ui/screens/timetable/events/block.dart
+++ lib/ui/screens/timetable/events/block.dart
@@ -13,9 +13,7 @@ import 'package:material_ui/material_ui.dart';
 import '../../../utils/utils.dart';
 
 part 'class/event.dart';
-
 part 'meal/event.dart';
-
 part 'pause/event.dart';
 
 typedef DayBlocks = List<Block>;
@@ -224,7 +222,7 @@ class BlockWidget extends StatefulWidget {
 }
 
 class _BlockWidgetState extends State<BlockWidget> {
-  int configurationIndex = 0;
+  int blockIndex = 0;
 
   Widget _buildWidgetConfiguration(
     BuildContext context,
@@ -282,7 +280,7 @@ class _BlockWidgetState extends State<BlockWidget> {
       );
     }
 
-    return Column(key: ValueKey(configurationIndex), children: display);
+    return Column(key: ValueKey(blockIndex), children: display);
   }
 
   @override
@@ -294,82 +292,87 @@ class _BlockWidgetState extends State<BlockWidget> {
       );
     }
 
-    return Row(
-      crossAxisAlignment: .stretch,
-      children: [
-        Expanded(
-          flex: 89,
-          child: AnimatedSwitcher(
-            duration: const Duration(milliseconds: 300),
-            reverseDuration: const Duration(milliseconds: 50),
-            switchInCurve: Curves.fastOutSlowIn,
-            transitionBuilder: (child, animation) {
-              return FadeTransition(opacity: animation, child: child);
-            },
-            child: IndexedStack(
-              key: ValueKey(configurationIndex),
-              index: configurationIndex,
-              children: [
-                for (final configuration in widget.block.configurations)
-                  _buildWidgetConfiguration(context, configuration),
-              ],
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const .all(.circular(20)),
+        color: context.c.surfaceContainerLow,
+      ),
+      child: Row(
+        crossAxisAlignment: .stretch,
+        children: [
+          Expanded(
+            flex: 89,
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              reverseDuration: const Duration(milliseconds: 50),
+              switchInCurve: Curves.fastOutSlowIn,
+              transitionBuilder: (child, animation) {
+                return FadeTransition(opacity: animation, child: child);
+              },
+              child: IndexedStack(
+                key: ValueKey(blockIndex),
+                index: blockIndex,
+                children: [
+                  for (final configuration in widget.block.configurations)
+                    _buildWidgetConfiguration(context, configuration),
+                ],
+              ),
             ),
           ),
-        ),
-        Expanded(
-          flex: 11,
-          child: Container(
-            padding: const .only(left: 4),
-            decoration: BoxDecoration(
-              borderRadius: .horizontal(right: const .circular(20)),
-              color: context.c.surfaceContainerLow,
-            ),
-            clipBehavior: .antiAlias,
-            child: Column(
-              children: [
-                ...widget.block.configurations.mapIndexed((i, _) {
-                  final borderRadius = BorderRadius.only(
-                    bottomRight: i == 0 ? .zero : const .circular(16),
-                    topLeft: i == 0 ? const .circular(4) : .zero,
-                    topRight: i == widget.block.configurations.length - 1
-                        ? .zero
-                        : const .circular(16),
-                    bottomLeft: i == widget.block.configurations.length - 1
-                        ? const .circular(4)
-                        : .zero,
-                  );
+          Expanded(
+            flex: 11,
+            child: Container(
+              padding: const .only(left: 4),
+              decoration: BoxDecoration(
+                borderRadius: .horizontal(right: const .circular(20)),
+              ),
+              clipBehavior: .antiAlias,
+              child: Column(
+                children: [
+                  ...widget.block.configurations.mapIndexed((i, _) {
+                    final borderRadius = BorderRadius.only(
+                      bottomRight: i == 0 ? .zero : const .circular(16),
+                      topLeft: i == 0 ? const .circular(4) : .zero,
+                      topRight: i == widget.block.configurations.length - 1
+                          ? .zero
+                          : const .circular(16),
+                      bottomLeft: i == widget.block.configurations.length - 1
+                          ? const .circular(4)
+                          : .zero,
+                    );
 
-                  return Expanded(
-                    child: Pressable(
-                      borderRadius: borderRadius,
-                      onPressed: () {
-                        if (i == configurationIndex) return;
+                    return Expanded(
+                      child: Pressable(
+                        borderRadius: borderRadius,
+                        onPressed: () {
+                          if (i == blockIndex) return;
 
-                        setState(() => configurationIndex = i);
-                      },
-                      child: Ink(
-                        decoration: BoxDecoration(
-                          borderRadius: borderRadius,
-                          color: i == configurationIndex
-                              ? context.c.secondaryContainer
-                              : null,
-                        ),
-                        child: Container(
-                          alignment: .center,
-                          child: Text(
-                            (i + 1).toString(),
-                            style: const TextStyle(fontWeight: .w800),
+                          setState(() => blockIndex = i);
+                        },
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            borderRadius: borderRadius,
+                            color: i == blockIndex
+                                ? context.c.secondaryContainer
+                                : null,
+                          ),
+                          child: Container(
+                            alignment: .center,
+                            child: Text(
+                              (i + 1).toString(),
+                              style: const TextStyle(fontWeight: .w800),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  );
-                }),
-              ],
+                    );
+                  }),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
